4b159fa86ac1a1bd43f72e7e87fa5733c1846bee
shaun-0 <virendersr2000@gmail.com>
2021-11-08 13:42:41 +0530

    dburl

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const games = require('./models/game');
 const methodOverride = require("method-override");
 
 const PORT = process.env.PORT || 4000;
-const mongodbString = process.env.DBURL || 'mongodb+srv://virendersingh12:falcon404@cluster0.kw5tn.mongodb.net/onlineGame?retryWrites=true&w=majority';  //mongo atlas database url
+const mongodbString = process.env.DBURL;  //mongo atlas database url
 
 app.set("view engine", "ejs");
 app.use(express.json());
